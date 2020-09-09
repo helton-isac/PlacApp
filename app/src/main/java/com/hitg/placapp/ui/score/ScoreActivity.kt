bc7f2f2bc6a90cb1649bd37bc17490286b6d81bc
Helton Isac <helton.isac@gmail.com>
2020-09-08 22:18:40 -0300

    Adding the Activity Score

---
+++ app/src/main/java/com/hitg/placapp/ui/score/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.hitg.placapp.ui.score
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hitg.placapp.R
+
+class ScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
