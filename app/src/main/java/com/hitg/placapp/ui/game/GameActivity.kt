e3a48db433a25888762274eb7fb0cc7796d2823a
Helton Isac <helton.isac@gmail.com>
2020-09-08 22:08:57 -0300

    Adding the EventFragment

---
+++ app/src/main/java/com/hitg/placapp/ui/game/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.hitg.placapp.ui.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hitg.placapp.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
